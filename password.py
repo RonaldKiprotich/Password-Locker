fa398010fc11c2f7fb6387a71917114843b4900b
RonaldKiprotich <protich12@gmail.com>
2020-10-11 09:14:32 +0300

    Added register function

--- password.py
+++ password.py
@@ -1,3 +1,25 @@
+persons = []
+
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def register():
+        person1 = User(input("Your Username: "), input("Your Password: "))
+
+
+
+
+
+
+
+
+
+
+
+
 print("Welcome to password locker")
 print("From the options below, choose lg for login, cr to create a new account and ex for exit")
 choice = input()
@@ -5,8 +27,11 @@ choice = input()
 if choice == "lg":
     print("login")
 elif choice == "cr":
-    print("create account")
+    User.register()
 elif choice == "ex":
     exit()
 else: 
     print("Invalid. Please try again")
+
+
+
