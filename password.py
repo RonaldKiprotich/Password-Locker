3e3cc4ff2725ee2d2c3d17b0df308d52b7647180
RonaldKiprotich <protich12@gmail.com>
2020-10-12 00:03:14 +0300

    save credentials

--- password.py
+++ password.py
@@ -1,32 +1,73 @@
-persons = []
 
+import random
+
+persons = list()
+cred_list = []
 
 class User:
+    def register():
+        person1 = User(input("Your Username: "), input("Your Password: "))
+        persons.append(person1)
+        User.login()
+
+ 
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
-    def register():
-        person1 = User(input("Your Username: "), input("Your Password: "))
-        persons.append("person1")
-        User.login()
-        
     def login():
         print("Put in your details to login")
         username1 = input("Username: ")
         password1 = input("Password: ")
+        #person_combined = username + password
+        # for obj in persons:
+        #     print()
+        # if username1 == person1.username and password1 == person1.password:
+        #     print("almost there")
+        # else:
+        #     print("matata")
+        Cred.saveCredential()
 
+class Cred():
+    def __init__ (self,accountname,username,password):
+        self.accountname = accountname
+        self.username = username
+        self.password = password
 
+    def saveCredential():
+        print ("To proceed choose the following. Choose the shortcodes: \n sc- Save Credential\n vc - View Credentials\n dc- Delete Credential ex- exit\n") 
+        option = input().lower()
 
+        if option == 'sc':
+            Print("Enter your Accounts credentials:\n")
+            print("Account name:")
+            accountname = input()
+            print("Username:")
+            username = input()
+            print("Will you like to en - enter your password or sg - system generated?")
+            passwordChoice = input.lower()
+            if passwordChoice == 'en':
+                password = input()
+            if passwordChoice == 'sg':
+                pass
+                
+            cred_list.append(accountname,username,password)     
+     
+            
+        elif option == 'vc':
+            print("view existing credentials")
+        
+        elif option == 'dc':
+            pass  
+        elif option == 'ex':
+            print("Bye")
 
+        else:
+            print("Invalid option")    
 
+    def generate_random_password():
 
-
-
-
-
-
-
+              
 
 
 print("Welcome to password locker")
